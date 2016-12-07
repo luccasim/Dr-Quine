b854b0404d048c403997f78407dff2c4cd49fc14
Luc CASIMIR <luccasim@e2r4p21.42.fr>
2016-12-07 13:15:09 +0100

    dr quine swift ex00-ex01

---
+++ drquine/bonus/ex00/Colleen.swift
@@ -0,0 +1,6 @@
+// Comment here
+func a(){/* another comment here */}
+let b = "// Comment here\nfunc a(){/* another comment here */}\n"
+let n = "\n"
+let c = "\nprint(b+\"let b = \"+b.debugDescription+n+\"let n = \"+n.debugDescription+n+\"let c = \"+c.debugDescription+c)"
+print(b+"let b = "+b.debugDescription+n+"let n = "+n.debugDescription+n+"let c = "+c.debugDescription+c)
