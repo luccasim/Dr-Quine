b5148724b629c2b9047a8bbf17a5f45976e15384
Luc CASIMIR <luccasim@e2r4p21.42.fr>
2016-12-13 17:46:36 +0100

    fini swift

--- drquine/bonus/ex01/Grace.swift
+++ drquine/bonus/ex01/Grace.swift
@@ -2,7 +2,7 @@ import Foundation
 /*
 	Swift is cool
 */
-let t = "\"import Foundation\\n/*\\n\\tSwift is cool\\n*/\\nlet t = \" + t.debugDescription + \"\\nlet text = \" + t + \"\\nlet dir = FileManager.default.currentDirectoryPath + \\\"/Grace_kid.swift\\\"\\ntry! text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)\""
-let text = "import Foundation\n/*\n\tSwift is cool\n*/\nlet t = " + t.debugDescription + "\nlet text = " + t + "\nlet dir = FileManager.default.currentDirectoryPath + \"/Grace_kid.swift\"\ntry! text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)"
+let t = "\"import Foundation\\n/*\\n\\tSwift is cool\\n*/\\nlet t = \" + t.debugDescription + \"\\nlet text = \" + t + \"\\nlet dir = FileManager.default.currentDirectoryPath + \\\"/Grace_kid.swift\\\"\\ntry? text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)\""
+let text = "import Foundation\n/*\n\tSwift is cool\n*/\nlet t = " + t.debugDescription + "\nlet text = " + t + "\nlet dir = FileManager.default.currentDirectoryPath + \"/Grace_kid.swift\"\ntry? text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)"
 let dir = FileManager.default.currentDirectoryPath + "/Grace_kid.swift"
-try! text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)
+try? text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)
--- drquine/bonus/ex02/Sully.swift
+++ drquine/bonus/ex02/Sully.swift
@@ -1,3 +1,24 @@
 import Foundation
-let a = "echo \"Hello world\n\""
-print(a)
+
+func task(cmd: String, arg: [String])
+{
+	let task = Process()
+	task.launchPath = cmd
+	task.arguments = arg
+	task.launch()
+	task.waitUntilExit()
+}
+
+var i = 5
+if i == 0{
+	exit(0)
+}
+let out = CommandLine.arguments[0]
+i = (out != "./Sully") ? i - 1 : i
+let filename = "Sully_" + String(i)
+let code = "\"import Foundation\\n\\nfunc task(cmd: String, arg: [String])\\n{\\n\\tlet task = Process()\\n\\ttask.launchPath = cmd\\n\\ttask.arguments = arg\\n\\ttask.launch()\\n\\ttask.waitUntilExit()\\n}\\n\\nvar i = \\(i)\\nif i == 0{\\n\\texit(0)\\n}\\nlet out = CommandLine.arguments[0]\\ni = (out != \\\"./Sully\\\") ? i - 1 : i\\nlet filename = \\\"Sully_\\\" + String(i)\\nlet code = \" + code.debugDescription + \"\\nlet text = \" + code + \"\\nlet dir = FileManager.default.currentDirectoryPath + \\\"/\\\\(filename).swift\\\"\\ntry? text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)\\ntask(cmd: \\\"/usr/bin/xcrun\\\", arg: [\\\"--sdk\\\",\\\"macosx\\\", \\\"swiftc\\\", filename+\\\".swift\\\"])\\ntask(cmd: filename, arg: [])\""
+let text = "import Foundation\n\nfunc task(cmd: String, arg: [String])\n{\n\tlet task = Process()\n\ttask.launchPath = cmd\n\ttask.arguments = arg\n\ttask.launch()\n\ttask.waitUntilExit()\n}\n\nvar i = \(i)\nif i == 0{\n\texit(0)\n}\nlet out = CommandLine.arguments[0]\ni = (out != \"./Sully\") ? i - 1 : i\nlet filename = \"Sully_\" + String(i)\nlet code = " + code.debugDescription + "\nlet text = " + code + "\nlet dir = FileManager.default.currentDirectoryPath + \"/\\(filename).swift\"\ntry? text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)\ntask(cmd: \"/usr/bin/xcrun\", arg: [\"--sdk\",\"macosx\", \"swiftc\", filename+\".swift\"])\ntask(cmd: filename, arg: [])"
+let dir = FileManager.default.currentDirectoryPath + "/\(filename).swift"
+try? text.write(toFile: dir, atomically: false, encoding: String.Encoding.utf8)
+task(cmd: "/usr/bin/xcrun", arg: ["--sdk","macosx", "swiftc", filename+".swift"])
+task(cmd: filename, arg: [])
