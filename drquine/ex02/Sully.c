e0763177d656a98193c54e2f1e44c9a17f2004da
Luc CASIMIR <luccasim@e2r3p19.42.fr>
2016-12-15 15:21:59 +0100

    swift is so fast:

--- drquine/ex02/Sully.c
+++ drquine/ex02/Sully.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define CODE "#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#define CODE %c%s%c%cint main(int ac, char **av)%c{%c%cchar name[100];%c%cint i = %d;%c%cFILE *f;%c%c%cif (i<=0)%c%c%creturn(0);%c%cac = (strcmp(%c./Sully%c,av[0]))? i - 1 : i;%c%csprintf(name,%cSully_%%d.c%c,ac);%c%cf = fopen(name,%cw%c);%c%cfprintf(f,CODE,10,10,10,34,CODE,34,10,10,10,9,10,9,ac,10,9,10,10,9,10,9,9,10,9,34,34,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,34,34,10,9,10,9,10);%c%cfclose(f);%c%cchar cmd[100];%c%csprintf(cmd, %cclang -Wall -Wextra -Werror %%s -o Sully_%%d && ./Sully_%%d%c,name,ac,ac);%c%csystem(cmd);%c%creturn (0);%c}"
+#define CODE "#include <stdio.h>%c#include <stdlib.h>%c#include <string.h>%c#define CODE %c%s%c%cint main(int ac, char **av)%c{%c%cchar name[100];%c%cint i = %d;%c%cFILE *f;%c%c%cif (i<=0)%c%c%creturn(0);%c%cac = (strstr(av[0],%cSully_%c))? i - 1 : i;%c%csprintf(name,%cSully_%%d.c%c,ac);%c%cf = fopen(name,%cw%c);%c%cfprintf(f,CODE,10,10,10,34,CODE,34,10,10,10,9,10,9,ac,10,9,10,10,9,10,9,9,10,9,34,34,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,34,34,10,9,10,9,10);%c%cfclose(f);%c%cchar cmd[100];%c%csprintf(cmd, %cclang -Wall -Wextra -Werror %%s -o Sully_%%d && ./Sully_%%d%c,name,ac,ac);%c%csystem(cmd);%c%creturn (0);%c}"
 int main(int ac, char **av)
 {
 	char name[100];
@@ -10,7 +10,7 @@ int main(int ac, char **av)
 
 	if (i<=0)
 		return(0);
-	ac = (strcmp("./Sully",av[0]))? i - 1 : i;
+	ac = (strstr(av[0],"Sully_"))? i - 1 : i;
 	sprintf(name,"Sully_%d.c",ac);
 	f = fopen(name,"w");
 	fprintf(f,CODE,10,10,10,34,CODE,34,10,10,10,9,10,9,ac,10,9,10,10,9,10,9,9,10,9,34,34,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,34,34,10,9,10,9,10);
