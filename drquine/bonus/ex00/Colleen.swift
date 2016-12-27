5846143be372362f3966af053bb8bedc7e8c76e1
luccasim <luccasim@e2r4p21.42.fr>
2016-12-27 14:57:45 +0100

    git push origin master

--- drquine/bonus/ex00/Colleen.swift
+++ drquine/bonus/ex00/Colleen.swift
@@ -6,7 +6,8 @@ func a(){
 	another comment here
 */
 }
-let b = "/*\n\tComment here\n*/\nfunc a(){\n/*\n\tanother comment here\n*/\n}\nlet b = "
+a()
+let b = "/*\n\tComment here\n*/\nfunc a(){\n/*\n\tanother comment here\n*/\n}\na()\nlet b = "
 let n = "\n"
 let c = "\nprint(b + b.debugDescription + n + \"let n = \" + n.debugDescription + n + \"let c = \" + c.debugDescription + c)"
 print(b + b.debugDescription + n + "let n = " + n.debugDescription + n + "let c = " + c.debugDescription + c)
--- drquine/ex00/Colleen.c
+++ drquine/ex00/Colleen.c
@@ -2,15 +2,16 @@
 /*
 	Salut
 */
-void fin(void){
+int fin(void){
 /*
 	Hello World
 */
+	return(0);
 }
 
 int main(void)
 {
-	char	*a="#include <stdio.h>%c/*%c%cSalut%c*/%cvoid fin(void){%c/*%c%cHello World%c*/%c}%c%cint main(void)%c{%c%cchar%c*a=%c%s%c;%c%cprintf(a,10,10,9,10,10,10,10,9,10,10,10,10,10,10,9,9,34,a,34,10,9,10,9,10,9);%c%creturn(0);%c}";
-	printf(a,10,10,9,10,10,10,10,9,10,10,10,10,10,10,9,9,34,a,34,10,9,10,9,10,9);
-	return(0);
+	char	*a="#include <stdio.h>%c/*%c%cSalut%c*/%cint fin(void){%c/*%c%cHello World%c*/%c%creturn(0);%c}%c%cint main(void)%c{%c%cchar%c*a=%c%s%c;%c%cprintf(a,10,10,9,10,10,10,10,9,10,10,9,10,10,10,10,10,9,9,34,a,34,10,9,10,9,10,9);%c%creturn(fin());%c}";
+	printf(a,10,10,9,10,10,10,10,9,10,10,9,10,10,10,10,10,9,9,34,a,34,10,9,10,9,10,9);
+	return(fin());
 }
